a61288349e58397ccff8a40f5c20d29fd1e1068b
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2021-11-19 14:25:09 +0000

    Hello world cube

--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,24 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshBasicMaterial( { color: 0x00ff00 }),
+)
+
+scene.add(cube);
+
+camera.position.z = 5;
+
+function render() {
+    requestAnimationFrame(render);
+    
+
+    cube.rotation.x -= 0.01;
+    cube.rotation.z += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+render();
